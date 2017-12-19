338d37cd4ce8e6e0051a51f8249537a71499358c
Brendan Weibrecht <brendan@weibrecht.net.au>
2017-12-19 19:14:53 +1100

    Remove debug logging and fix robctl command watching

--- lib/bot/world.rb
+++ lib/bot/world.rb
@@ -17,28 +17,16 @@ module Bot
     end
 
     def process event
-      p 10
       e = event["event"]
       robot_name = event["robot"]
       case event["type"]
       when "place"
-        p 11
-        p e
-        p 12
         p({x: e["x"], y: e["y"], direction: e["direction"]})
-        p 13
         state = Bot::State.new(x: e["x"], y: e["y"], direction: e["direction"])
-        p 14
-        p state
-        p 15
-        p @robots[robot_name]
-        p 16
         @robots[robot_name] = state
-        p 17
       when "left", "right", "move"
         @robots[robot_name] = @robots[robot_name].send event["type"]
       end
-      p 13
     end
   end
 end
